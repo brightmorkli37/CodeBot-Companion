153bf28aff44e578618d4414bd123d152597ef02
brightmorkli37 <brightmorkli37@gmail.com>
2023-03-02 16:08:45 +0000

    add code submit logic

--- codebot/views.py
+++ codebot/views.py
@@ -2,6 +2,24 @@ from django.shortcuts import render
 
 
 def homepage(request):
+    lang_list = [
+                    'c', 'clike', 'cpp', 'csharp', 'css', \
+                    'dart', 'go', 'html', 'java', 'javascript', \
+                    'julia', 'kotlin', 'markup', 'markup-templating', \
+                    'matlab', 'mongodb', 'objectivec', 'perl', 'php', \
+                    'plsql', 'powershell', 'python', 'r', 'ruby', 'sass', \
+                    'scala', 'sql', 'swift', 'typescript' \
+                ]
+
+    code = ''
+    lang = ''
+    if request.method == 'POST':
+        code = request.POST['code']
+        lang = request.POST['lang']
+
     template_name = 'codebot/homepage.html'
-    context = {}
+    context = {
+        'lang_list': lang_list, 'code': code,
+        'lang': lang,
+    }
     return render(request, template_name, context)
